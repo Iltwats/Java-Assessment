a3341d276c49ad591f51ae744fd94da4dc9dd9a6
Atul Sharma <2012atulsharma@gmail.com>
2021-10-14 15:11:01 +0530

    Some refactoring and javadocs added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 /**
  * Driver Code for Testing the Throttle Implementation
+ * @author Atul Sharma
+ * @since 13-10-21
  */
 public class Main {
     public static void main(String[] args) {
         ThrottleController throttleController = new ThrottleControllerImpl();
         throttleController.setCruiseSpeed(34);
-        Integer getCurrentSpeed = 12;
+        Integer getCurrentSpeed = 14;
         final Integer throttleControlValue = throttleController.calculateThrottle(getCurrentSpeed);
         System.out.println(throttleControlValue);
-        // setThrottleValue(throttleControlValue);
+        // set the current throttle value
+        throttleController.setThrottleValue(throttleControlValue);
+        Integer getCurrentSpeed1 = 7;
+        // after speed changes again calculate throttle value
+        final Integer throttleControlValue1 = throttleController.calculateThrottle(getCurrentSpeed1);
+        System.out.println(throttleControlValue1);
     }
 }
--- src/main/java/Quantizer.java
+++ src/main/java/Quantizer.java
@@ -1,10 +1,12 @@
 /**
  * Generic interface to quantize any datatype passed into it
+ *
  * @param <T>
  */
 public interface Quantizer<T> {
     /**
      * Calculate quantized value of throttle
+     *
      * @param quantity is passed as vehicle speed
      * @return the quantized value of throttle
      */
